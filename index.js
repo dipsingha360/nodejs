2d09561a1779d6bf9315e40a9b6826703cb5c003
dipsingha360 <dipsingha2@gmail.com>
2021-09-23 20:50:06 +0600

    test nodejs modules system on nodejs internal modules system

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const _ = require("lodash");
-let a = [40, 45, 2, 34, 0, 56];
-console.log(_.first(a));
-console.log(_.findLastIndex(a));
+const testIt = require("./test");
+const test = require("./test");
+const os = require("os");
+const fs = require("fs");
+
+// let a = [40, 45, 2, 34, 0, 56];
+// console.log(_.first(a));
+// console.log(_.findLastIndex(a));
+// console.log(global);
+
+// testIt();
+// console.log(os.memory());
+
+fs.writeFile("write.txt", "test write file", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("text write into write.txt");
+  }
+});
+console.log("print done");
+
+fs.readFile("./write.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
---
+++ test.js
@@ -0,0 +1,6 @@
+const test = [79, 50, 40];
+function testIt() {
+  console.log(test);
+}
+
+module.exports = testIt;
