6799a3e20347783c61b33792334e400b0f374a1a
dipsingha360 <dipsingha2@gmail.com>
2021-09-28 02:47:27 +0600

    play with file system in nodejs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const testIt = require("./test");
 const test = require("./test");
 const os = require("os");
 const fs = require("fs");
+const EventEmitter = require("events");
 
 // let a = [40, 45, 2, 34, 0, 56];
 // console.log(_.first(a));
@@ -19,12 +20,20 @@ fs.writeFile("write.txt", "test write file", function (err, data) {
     console.log("text write into write.txt");
   }
 });
-console.log("print done");
+console.log("Async test");
 
 fs.readFile("./write.txt", (err, data) => {
   if (err) {
     console.log(err);
   } else {
     console.log(data.toString());
+  }
+});
+
+fs.open("write.txt", function (err, data) {
+  if (err) {
+    return err;
+  } else {
+    return data.toString();
   }
 });
